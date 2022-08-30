76227ed4e754a88bb679344cf0e918ff6d5af384
Henrique Medeiros <henrique.medeiros@alunos.sc.senac.br>
2022-08-29 21:54:32 -0300

    Criada a classe calculo e metodo de impressao resultado

---
+++ src/poo/Calculo.java
@@ -0,0 +1,13 @@
+package poo;
+
+public class Calculo {
+
+    public static double elevar(double x) {
+        return x * x;
+
+    }
+      public static double elevar(double x, double y) {
+        return Math.pow(x, y);
+          
+      }
+}
--- src/poo/Poo.java
+++ src/poo/Poo.java
@@ -4,35 +4,42 @@ package poo;
 public class Poo {
 
     public static void main(String[] args) {
-      Carro meuCarro = new Carro();
-      
-      meuCarro.setCor("Preto");
-      meuCarro.setNumPortas(4);
-      meuCarro.setPlaca("YPM-4262");
-      meuCarro.setTipo("Corsa");
-      
-      meuCarro.imprimeDadosDoCarro();
-      
-      Pessoa humano = new Pessoa ();
-      humano.setNome("Jorjin do Pneu");
-      humano.setAltura("1.81");
-      humano.setCorDoCabelo("Preto");
-      humano.setBiotipo("Magro");
-      humano.setIdade(18);
-      
-      humano.imprimeDadosPessoa();
-      
-      Barco meubarco = new Barco();
-      meubarco.setNome("New Bering");
-      meubarco.setCombustivel("Diesel");
-      meubarco.setCor("Branco");
-      meubarco.setTamanho("250 Metros");
-      meubarco.setTipo("Pesqueiro");
-      meubarco.setAno(2008);
-      
-      
-      meubarco.imprimeDadosDoBarco();
         
+        double a = 3, b =2;
+        
+        
+        System.out.println("O resultado é: " + Calculo.elevar(a, b));
+        
+        
+        
+//      Carro meuCarro = new Carro();
+//      
+//      meuCarro.setCor("Preto");
+//      meuCarro.setNumPortas(4);
+//      meuCarro.setPlaca("YPM-4262");
+//      meuCarro.setTipo("Corsa");
+//      
+//      meuCarro.imprimeDadosDoCarro();
+//      
+//      Pessoa humano = new Pessoa ();
+//      humano.setNome("Jorjin do Pneu");
+//      humano.setAltura("1.81");
+//      humano.setCorDoCabelo("Preto");
+//      humano.setBiotipo("Magro");
+//      humano.setIdade(18);
+//      
+//      humano.imprimeDadosPessoa();
+//      
+//      Barco meubarco = new Barco();
+//      meubarco.setNome("New Bering");
+//      meubarco.setCombustivel("Diesel");
+//      meubarco.setCor("Branco");
+//      meubarco.setTamanho("250 Metros");
+//      meubarco.setTipo("Pesqueiro");
+//      meubarco.setAno(2008);
+//      
+//      
+//      meubarco.imprimeDadosDoBarco();       
        
     }
     
